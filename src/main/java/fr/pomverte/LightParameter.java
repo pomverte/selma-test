4781c3bebce4e87c3a8a2c4f097e26743c2583eb
hvle <ha.hong.viet.le@cgi.com>
2016-08-23 14:24:05 +0200

    Lombok and Selma are not friends

--- src/main/java/fr/pomverte/LightParameter.java
+++ src/main/java/fr/pomverte/LightParameter.java
@@ -1,13 +1,23 @@
 package fr.pomverte;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@ToString
 public class LightParameter {
     private String user;
     private String password;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
--- src/main/java/fr/pomverte/RichParameter.java
+++ src/main/java/fr/pomverte/RichParameter.java
@@ -1,15 +1,49 @@
 package fr.pomverte;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@Builder
 public class RichParameter {
     private String user;
     private String password;
     private String adress;
     private String language;
+
+    public RichParameter(String user, String password, String adress, String language) {
+        super();
+        this.user = user;
+        this.password = password;
+        this.adress = adress;
+        this.language = language;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
 }
